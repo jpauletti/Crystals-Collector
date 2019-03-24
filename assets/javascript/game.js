af0e6e49f4a8849fc16ef52fe03560a7c18cfb1d
Julia Pauletti <julia.pauletti@yahoo.com>
2019-03-24 13:23:26 -0500

    style changes, added message, cleaned up code, fixed ending

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,43 @@
+var game = {
+
+};
+
+
+
 var $crystals = $(".crystal");
 var $targetNumber = $("#target-number");
 var $score = $("#score");
 var $wins = $("#wins");
 var $losses = $("#losses");
+var $message = $("#message");
 
 
-// var crystalPoints = [];
 var wins = 0;
 var losses = 0;
 var score = 0;
 var targetNumber = 0;
+var gameOver = false;
 
 function startGame() {
     // get random target number
     targetNumber = Math.floor((Math.random() * 102) + 19);
     // display target number on page
     $targetNumber.text(targetNumber);
-    console.log("Target Number: " + targetNumber);
+    console.log("game started");
 
-    // reset score and each crystal's points
-    // newCrystalPoints();
+    // reset score, gameOver, message, and each crystal's points
     setCrystalValues();
     score = 0;
     $score.text("0");
+    $message.empty();
+    gameOver = false;
 }
 
-// function newCrystalPoints() {
-//     for (var i = 1; i < 5; i++) {
-//         var randomCrystalNumber = Math.floor((Math.random() * 12) + 1);
-//         crystalPoints.push(randomCrystalNumber);
-//     }
-//     console.log(crystalPoints);
-// }
-
 function setCrystalValues() {
-    // assign data-points values for each crystal using crystalPoints array
     $.each($crystals, function (i, crystal) {
-        // var points = crystalPoints[i];
+        // calculate random number
         var randomCrystalNumber = Math.floor((Math.random() * 12) + 1);
-        // crystalPoints.push(randomCrystalNumber);
+        // assign the random number to the crystal
         $(this).attr("data-points", randomCrystalNumber);
         console.log("Crystal Number " + i + ": " + randomCrystalNumber);
     })
@@ -49,14 +48,17 @@ function youLose() {
     console.log("loser");
     losses += 1;
     $losses.text(losses);
-    startGame();
+    $message.text("Better luck next time! Want to play again?");
+    gameOver = true;
 }
 
 function youWin() {
     console.log("winner");
-    wins +=1;
+    wins += 1;
     $wins.text(wins);
-    startGame();
+    $message.text("You won! Let's play again.");
+    // startGame();
+    gameOver = true;
 }
 
 
@@ -71,28 +73,20 @@ startGame();
 
 
 $crystals.on("click", function() {
-    // grab points, add to score
-    score += Number($(this).attr("data-points"));
-    // update score on page
-    $score.text(score);
 
-    // decide if win or lose
-    if (targetNumber === score) {
-        youWin();
-    } else if (targetNumber < score) {
-        youLose();
+    if (gameOver) {
+        startGame();
+    } else {
+        // grab points, add to score
+        score += Number($(this).attr("data-points"));
+        // update score on page
+        $score.text(score);
+
+        // decide if win or lose
+        if (targetNumber === score) {
+            youWin();
+        } else if (targetNumber < score) {
+            youLose();
+        }
     }
 });
-
-
-
-
-// generate random winning number (19 - 120)
-// generate random numbers for each crystal (1 - 12)
-    // each time a user clicks a crystal, that number is added to their score
-
-// when you guess the number, wins++
-
-
-// if user's score goes above the winning number, they lose, loses++
-// when user wins or loses, game resets with new random numbers
